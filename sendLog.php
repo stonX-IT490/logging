840c25e5c60cbd2abc794b95fa635459623ca3da
Aman <amanfromoakland@gmail.com>
2022-03-20 18:55:57 -0400

    ci: create function to send logs over rmq

---
+++ sendLog.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$exchange = $argv[1];
+$dir = '/var/log/'.$exchange;
+
+$connection = new AMQPStreamConnection('10.4.90.102', 5672, 'log', 'stonx_log', 'logHost');
+$channel = $connection->channel();
+
+$channel->exchange_declare($exchange, 'fanout', false, false, false);
+$channel->queue_declare('log', false, true, false, false);
+
+$file = escapeshellarg( $dir );
+$last_line = `tail -n 1 $file`;
+
+$msg = new AMQPMessage($last_line);
+
+$channel->basic_publish($msg, $exchange);
+
+echo ' [x] Sent ', $dir, "\n";
+$channel->close();
+$connection->close();
+
+?>
