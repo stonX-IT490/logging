b6f6f3ddafb7261adbf2d5b1161cb1284db527fd
Aman <amanfromoakland@gmail.com>
2022-03-21 03:27:49 -0400

    fix: composer not installing fixed

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # Composer
 cd ~/logging/
-sudo -i wget -O composer-setup.php https://getcomposer.org/installer
+sudo wget -O composer-setup.php https://getcomposer.org/installer
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 composer require php-amqplib/php-amqplib
 composer update
