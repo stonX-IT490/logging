8f5e32ca9eb6e6a70505dad13acadc67360a9f81
Aman <amanfromoakland@gmail.com>
2022-05-09 07:08:00 -0400

    fix: loop

--- failover.sh
+++ failover.sh
@@ -2,10 +2,13 @@
 primary="10.4.90.52"
 secondary="10.4.90.62"
 
-if ping -c 1 -W 1 $primary; then
-  echo $primary
-  sed -i "s/$secondary/$primary/" /etc/hosts
-else
-  echo $secondary
-  sed -i "s/$primary/$secondary/" /etc/hosts
-fi
+while true; do
+    if ping -c 1 -W 1 $primary; then
+    echo $primary
+    sed -i "s/$secondary/$primary/" /etc/hosts
+    else
+    echo $secondary
+    sed -i "s/$primary/$secondary/" /etc/hosts
+    fi
+    sleep 5s
+done
